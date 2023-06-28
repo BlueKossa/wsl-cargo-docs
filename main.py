7f9c2acfa67e8c52cd3577b23781dd6935cb4449
BlueKossa <dexter@stormdal.se>
2023-06-28 02:31:02 +0200

    Changed it to not use a subprocess ig

--- main.py
+++ main.py
@@ -2,16 +2,15 @@
 from subprocess import PIPE, run, Popen
 from threading import Thread, Event
 from sys import argv
+import http.server
 
-event = Event()
+killer = Event()
 
-def run_server(port):
-    cmd = ["npx", "serve", "-n", "-p", str(port), "./target/doc"]
-    server = Popen(cmd, stdout=PIPE, universal_newlines=True)
-    event.wait()
-    server.kill()
-    
-    
+class DirHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        # Idc some dumb server shit does not work if it is directly in the
+        # doc folder
+        super().__init__(*args, directory="./target", **kwargs)
 
 
 def get_project_name():
@@ -25,8 +24,10 @@ def get_project_name():
 
 
 def main():
-    port = argv[1] if len(argv) > 1 else 2048
-    server = Thread(target=run_server, args=(port,))
+    port = int(argv[1]) if len(argv) > 1 else 2048
+    handler = DirHandler
+    httpd = http.server.HTTPServer(("", port), handler)
+    server = Thread(target=httpd.serve_forever)
     server.start()
 
     project_name = get_project_name()
@@ -34,17 +35,16 @@ def main():
     no_color = "\033[0m"
     print(f"Server running at {color}http://localhost:{port}{no_color}")
     if project_name:
-        print(f"Project docs at {color}http://localhost:{port}/{project_name}{no_color}\n")
+        print(f"Project docs at {color}http://localhost:{port}/doc/{project_name}{no_color}\n")
     
     c = ""
     while c != "q":
         run(["cargo", "doc"])
         c = input("Reload? (q to quit) ").strip()
     print("Shutting down...")
-    event.set()
+    httpd.shutdown()
     server.join()
-    
-
  
+
 if __name__ == "__main__":
     main()
